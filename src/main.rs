25519301f79a2831ee1ec2410509b7fdeb005213
ka_ueno <ka.ueno@acca-int.co.jp>
2020-10-17 13:14:08 +0900

    .

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,37 @@
+extern crate rand;
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("guess the number !");
-    println!("please input your guess !");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("failed to read");
-    println!("you guessed : {}",guess);
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    let mut counter = 0;
+    loop {
+        counter += 1;
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = guess
+            .trim()
+            .parse()
+            .expect("Please type a positive integer!"); //数値を入力してください！
+
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),  //小さすぎ！
+            Ordering::Greater => println!("Too big!"), //大きすぎ！
+            Ordering::Equal => {
+                println!("You win!");
+                println!("you can find numbers in {} times!",counter);
+                break;
+            },                  //やったね！
+        }
+    }
 }
